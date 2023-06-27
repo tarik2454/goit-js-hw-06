1685252b996ba969028cdebc416b5918e1b93981
Taras Kovalyk <tarik2454@gmail.com>
2023-06-27 16:33:27 +0300

    Сorrects errors in the first task

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const categoriesList = document.querySelectorAll('li.item');
 
 console.log(`Number of categories: ${categoriesList.length}`);
 
-[...categoriesList].map(categorie => {
-  console.log(`Category: ${categorie.querySelector('h2').textContent}`);
-  console.log(`Elements: ${categorie.querySelector('ul').children.length}`);
+categoriesList.forEach(element => {
+  console.log(`Category: ${element.querySelector('h2').textContent}`);
+  console.log(`Elements: ${element.querySelector('ul').children.length}`);
 });
