0dd23dc0ae444daee66e02c49b1bb4b6bfde20ab
Taras Kovalyk <tarik2454@gmail.com>
2023-07-02 14:14:26 +0300

    Corrects errors in the fifth task

--- js/task-05.js
+++ js/task-05.js
@@ -4,7 +4,5 @@ const nameOutput = document.querySelector('#name-output');
 nameInput.addEventListener('input', onInputChange);
 
 function onInputChange({ target: { value } }) {
-  const text = (nameOutput.textContent = 'Anonymous');
-
-  nameOutput.textContent = value || text;
+  nameOutput.textContent = value || 'Anonymous';
 }
